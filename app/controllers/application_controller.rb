dde153835d1495b3c4d8b621131e7b467040a4dc
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2015-12-02 15:11:19 +0100

    All's working, cleaning needed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
 
   def do_NOT_protect_from_CORS
-l    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
     headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = '*'
